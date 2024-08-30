a1f66ea65e887f6f89484faa9ed334e4733eea71
isaacbrds <isaac.brigido@gmail.com>
2024-08-30 20:40:51 -0300

    feat: criando o primeiro crud

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,43 @@
+class CustomersController < ApplicationController
+  before_action :load_customer, only: [:show, :edit, :update, :destroy] 
+  def index
+    @customers = Customer.order(:name)
+  end
+
+  def show;end
+
+  def new
+    @customer = Customer.new
+  end
+
+  def create
+    @customer = Customer.new customer_params
+    if @customer.save
+      redirect_to customers_path, notice: 'Cliente cadastrado com sucesso!'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  def edit;end
+  def update
+    if @customer.update customer_params
+      redirect_to customers_path, notice: 'Cliente atualizado com sucesso!'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  def destroy
+    @customer.destroy
+    redirect_to customers_path
+  end
+
+  private
+
+  def load_customer
+    @customer = Customer.find(params[:id])
+  end
+
+  def customer_params
+    params.require(:customer).permit(:name, :dob, :email, :mobile_phone)
+  end
+end
